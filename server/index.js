d90d5d7811d79022d6e2201a596d070babeefe00
arpnghosh <arpnghosh@protonmail.com>
2023-08-30 22:37:53 +0530

    fix: deployment error

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://colabcodertest.vercel.app",
+    origin: "https://colabcoder.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
